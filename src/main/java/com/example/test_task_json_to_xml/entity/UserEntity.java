24ed9d30ce4a45b9a89cbc9dcb57175497367c78
Gomazkova Yulia <gomazkova19@yandex.ru>
2022-06-27 13:07:40 +0300

    Add reference on document entity

--- src/main/java/com/example/test_task_json_to_xml/entity/UserEntity.java
+++ src/main/java/com/example/test_task_json_to_xml/entity/UserEntity.java
@@ -1,6 +1,7 @@
 package com.example.test_task_json_to_xml.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "users", schema = "user")
 public class UserEntity {
@@ -28,6 +30,10 @@ public class UserEntity {
     @Column(name = "gender", nullable = false)
     @Enumerated(EnumType.ORDINAL)
     private Gender gender;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "document_id", nullable = false, referencedColumnName = "id")
+    private DocumentEntity document;
 
     @Column(name = "birthDate", nullable = false)
     private LocalDate birthDate;
