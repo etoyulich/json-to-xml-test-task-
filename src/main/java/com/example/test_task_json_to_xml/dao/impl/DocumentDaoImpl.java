7e09152ec4ba83e7a81bdf6b294736e0e74d6ec5
Yulia Gomazkova <gomazkova19@yandex.ru>
2022-07-12 18:04:51 +0300

    now it works with session factory

--- src/main/java/com/example/test_task_json_to_xml/dao/impl/DocumentDaoImpl.java
+++ src/main/java/com/example/test_task_json_to_xml/dao/impl/DocumentDaoImpl.java
@@ -2,7 +2,9 @@ package com.example.test_task_json_to_xml.dao.impl;
 
 import com.example.test_task_json_to_xml.dao.DocumentDao;
 import com.example.test_task_json_to_xml.entity.DocumentEntity;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -14,20 +16,23 @@ public class DocumentDaoImpl implements DocumentDao {
 
     private final EntityManager entityManager;
 
-    //private final SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
 
     @Autowired
     public DocumentDaoImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
-        //this.sessionFactory = sessionFactory;
+       // this.sessionFactory = sessionFactory;
     }
 
 
     @Override
-    @Transactional
     public void save(DocumentEntity document){
-        //sessionFactory.getCurrentSession().save(document);
-        entityManager.persist(document);
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(document);
+        transaction.commit();
+        session.close();
     }
 }
