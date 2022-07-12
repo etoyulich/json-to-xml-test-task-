dd317e49f0820780f6a9713ce7a3b4e9f3f0b97a
Yulia Gomazkova <gomazkova19@yandex.ru>
2022-07-12 17:14:48 +0300

    added validation

--- src/main/java/com/example/test_task_json_to_xml/dto/ClientCreationDto.java
+++ src/main/java/com/example/test_task_json_to_xml/dto/ClientCreationDto.java
@@ -5,23 +5,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class ClientCreationDto {
-
+    @NotBlank(message = "Name is mandatory")
     private String name;
 
+    @NotBlank(message = "Surname is mandatory")
     private String surname;
 
     private String patronymic;
 
+    @NotNull(message = "Birthdate is mandatory")
     private LocalDate birthDate;
 
+    @NotNull(message = "Gender is mandatory")
     private Gender gender;
 
+    @NotNull(message = "Document is mandatory")
     private DocumentDto document;
 
     @Override
--- src/main/java/com/example/test_task_json_to_xml/dto/DocumentDto.java
+++ src/main/java/com/example/test_task_json_to_xml/dto/DocumentDto.java
@@ -4,19 +4,27 @@ import com.example.test_task_json_to_xml.entity.DocumentType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
 public class DocumentDto {
 
+    @NotBlank(message = "Series is mandatory")
     private String series;
 
+    @NotBlank(message = "Number is mandatory")
     private String number;
 
+    @NotNull(message = "Issue date is mandatory")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate issueDate;
 
+    @NotNull(message = "Document type is mandatory")
     private DocumentType type;
 
     @Override
