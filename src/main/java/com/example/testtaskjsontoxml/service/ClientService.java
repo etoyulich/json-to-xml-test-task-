65029f3e15b1361c8820ccbcdeb5596ca54df724
Yulia Gomazkova <gomazkova19@yandex.ru>
2022-07-22 12:46:11 +0300

    updated logger

--- src/main/java/com/example/testtaskjsontoxml/service/ClientService.java
+++ src/main/java/com/example/testtaskjsontoxml/service/ClientService.java
@@ -10,8 +10,6 @@ import com.example.testtaskjsontoxml.service.newgen.GetClientResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
